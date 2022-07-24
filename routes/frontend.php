4628f5183d129cab3f3662d38a9f74f15f4f60d6
subratamandal <sm930365@gmail.com>
2022-07-24 14:48:50 +0530

    small changes

--- routes/frontend.php
+++ routes/frontend.php
@@ -6,8 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'  =>  'frontend'], function () {
     Route::controller(UserManagementController::class)->group(function () {
         Route::get('/users', 'index')->name('user.list');
-        Route::get('/test', function () {
-            return "hello";
-        });
     });
 });
